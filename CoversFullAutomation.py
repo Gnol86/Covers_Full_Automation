016c819dd89c11801c671cbbc15f46f898c6596b
Gnol86 <arnaud.marchot@icloud.com>
2022-10-14 00:03:35 +0200

    Update CoversFullAutomation.py

--- CoversFullAutomation.py
+++ CoversFullAutomation.py
@@ -111,7 +111,11 @@ class CoversFullAutomation(hass.Hass):
       self.debug_log(f'{entity} : {old} -> {new}')
       listCovers = {}
       for cover in self.covers:
-        if kwargs["trigger_type"] == "sun_elevation":
+        if  (
+            kwargs["trigger_type"] == "sun_elevation" or 
+            kwargs["trigger_type"] == "alarms" or 
+            kwargs["trigger_type"] == "presence"
+            ):
           listCovers[cover] = self.covers[cover]
         elif entity in self.covers[cover][kwargs["trigger_type"]]:
           listCovers[cover] = self.covers[cover]
